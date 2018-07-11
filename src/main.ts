34eac4e2fe02ea920a9db49288f6f88c5a299af5
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2018-07-11 11:01:36 -0700

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,49 @@
+// import arcgisPortal from "esri/arcgis/Portal";
+import BasemapGallery from "esri/dijit/BasemapGallery";
+import Extent from "esri/geometry/Extent";
+import EsriMap from "esri/map";
+
+// const portal = new arcgisPortal.Portal(undefined as any);
+// portal.basemapGalleryGroupQuery =
+//   'group:a89e08f2cc584e55a23b76fa7c9b8618 AND type:"web map"';
+
+/**
+ * Extent of WA.
+ * @see https://epsg.io/1416-area
+ */
+const extent = new Extent({
+  xmin: -124.79,
+  ymin: 45.54,
+  xmax: -116.91,
+  ymax: 49.05
+});
+
+const map = new EsriMap("map", {
+  basemap: "streets",
+  center: extent.getCenter(),
+  zoom: 7
+});
+
+// add the basemap gallery, in this case we'll display maps from ArcGIS.com including bing maps
+const basemapGallery = new BasemapGallery(
+  {
+    showArcGISBasemaps: false,
+    basemapsGroup: { id: "a89e08f2cc584e55a23b76fa7c9b8618" },
+    map
+  },
+  "basemapGallery"
+);
+basemapGallery.startup();
+
+basemapGallery.on("load", ({ target }) => {
+  // Select the Streets basemap to match the basemap loaded prior to the Basemap Gallery's creation.
+  const basemaps = target.basemaps.filter(bm => bm.title.match(/^Streets$/));
+  if (basemaps && basemaps.length) {
+    target.select(basemaps[0].id);
+  }
+});
+
+basemapGallery.on("error", msg => {
+  // tslint:disable-next-line:no-console
+  console.log("basemap gallery error:  ", msg);
+});
